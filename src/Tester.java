190e9ab458470ab6f251c2b729fbf2f3e6c72a93
binarypizza <binarypizza@users.noreply.github.com>
2014-05-08 23:28:48 +1000

    Convert leading tabs to spaces
    Changing style from tab-based indentation to spaces: each indentation
    level is 4 spaces.

--- src/Tester.java
+++ src/Tester.java
@@ -1,9 +1,9 @@
 
 public class Tester {
-	public static void main(String[] args) {
-		Maze myMaze = new Maze(81, 51, 1);
-		System.out.println(myMaze.toString());
-		myMaze.drawToScreen();
-	}
+    public static void main(String[] args) {
+        Maze myMaze = new Maze(81, 51, 1);
+        System.out.println(myMaze.toString());
+        myMaze.drawToScreen();
+    }
 
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,118 +1,118 @@
 
 public class Tile {
-	static final public int WALL = 1;
-	static final public int SPACE = 0;
-	
-	private int value;
-	private int x;
-	private int y;
-	private Tile north;
-	private Tile east;
-	private Tile south;
-	private Tile west;
-	
-	public Tile(int newValue, int newX, int newY) {
-		value = newValue;
-		x = newX;
-		y = newY;
-		
-		north = null;
-		east = null;
-		south = null;
-		west = null;
-	}
-	
-	public int getValue() {
-		return value;
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
-	public Tile getNorth() {
-		return this.north;
-	}
-	public Tile getEast() {
-		return this.east;
-	}
-	public Tile getSouth() {
-		return this.south;
-	}
-	public Tile getWest() {
-		return this.west;
-	}
-	public Tile getNorth(int n) {
-		Tile t = this.north;
-		if (t == null) {
-			return t;
-		}
-		
-		int i = 1;
-		while (t.north != null && i < n) {
-			t = t.north;
-			i++;
-		}
-		return t;
-	}
-	public Tile getEast(int n) {
-		Tile t = this.east;
-		if (t == null) {
-			return t;
-		}
-		
-		int i = 1;
-		while (t.east != null && i < n) {
-			t = t.east;
-			i++;
-		}
-		return t;
-	}
-	public Tile getSouth(int n) {
-		Tile t = this.south;
-		if (t == null) {
-			return t;
-		}
-		
-		int i = 1;
-		while (t.south != null && i < n) {
-			t = t.south;
-			i++;
-		}
-		return t;
-	}
-	public Tile getWest(int n) {
-		Tile t = this.west;
-		if (t == null) {
-			return t;
-		}
-		
-		int i = 1;
-		while (t.west != null && i < n) {
-			t = t.west;
-			i++;
-		}
-		return t;
-	}
-	
-	public void setValue(int newValue) {
-		value = newValue;
-	}
-	public void setNorth(Tile newNorth) {
-		north = newNorth;
-	}
-	public void setEast(Tile newEast) {
-		east = newEast;
-	}
-	public void setSouth(Tile newSouth) {
-		south = newSouth;
-	}
-	public void setWest(Tile newWest) {
-		west = newWest;
-	}
-	
-	public String toString() {
-		return "(" + x + ", " + y + ") with value " + value;
-	}
+    static final public int WALL = 1;
+    static final public int SPACE = 0;
+    
+    private int value;
+    private int x;
+    private int y;
+    private Tile north;
+    private Tile east;
+    private Tile south;
+    private Tile west;
+    
+    public Tile(int newValue, int newX, int newY) {
+        value = newValue;
+        x = newX;
+        y = newY;
+        
+        north = null;
+        east = null;
+        south = null;
+        west = null;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public Tile getNorth() {
+        return this.north;
+    }
+    public Tile getEast() {
+        return this.east;
+    }
+    public Tile getSouth() {
+        return this.south;
+    }
+    public Tile getWest() {
+        return this.west;
+    }
+    public Tile getNorth(int n) {
+        Tile t = this.north;
+        if (t == null) {
+            return t;
+        }
+        
+        int i = 1;
+        while (t.north != null && i < n) {
+            t = t.north;
+            i++;
+        }
+        return t;
+    }
+    public Tile getEast(int n) {
+        Tile t = this.east;
+        if (t == null) {
+            return t;
+        }
+        
+        int i = 1;
+        while (t.east != null && i < n) {
+            t = t.east;
+            i++;
+        }
+        return t;
+    }
+    public Tile getSouth(int n) {
+        Tile t = this.south;
+        if (t == null) {
+            return t;
+        }
+        
+        int i = 1;
+        while (t.south != null && i < n) {
+            t = t.south;
+            i++;
+        }
+        return t;
+    }
+    public Tile getWest(int n) {
+        Tile t = this.west;
+        if (t == null) {
+            return t;
+        }
+        
+        int i = 1;
+        while (t.west != null && i < n) {
+            t = t.west;
+            i++;
+        }
+        return t;
+    }
+    
+    public void setValue(int newValue) {
+        value = newValue;
+    }
+    public void setNorth(Tile newNorth) {
+        north = newNorth;
+    }
+    public void setEast(Tile newEast) {
+        east = newEast;
+    }
+    public void setSouth(Tile newSouth) {
+        south = newSouth;
+    }
+    public void setWest(Tile newWest) {
+        west = newWest;
+    }
+    
+    public String toString() {
+        return "(" + x + ", " + y + ") with value " + value;
+    }
 }
