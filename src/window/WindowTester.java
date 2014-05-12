79efd795b689475e1d3d3a8617f0bfd1f6fb7928
icedtrees <e_equals_mass_speed_light_squared@hotmail.com>
2014-05-13 01:38:32 +1000

    Added window tester for basic home window

---
+++ src/window/WindowTester.java
@@ -0,0 +1,17 @@
+package window;
+
+/**
+ * @author icedtrees
+ *
+ */
+public class WindowTester {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        HomeWindow home = new HomeWindow();
+        home.show();
+    }
+
+}
