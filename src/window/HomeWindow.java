2aee4b5faeaaac4f5daf9ff0d3529dd87588c5aa
icedtrees <e_equals_mass_speed_light_squared@hotmail.com>
2014-05-13 01:32:32 +1000

    added package declarations back into window package

--- src/window/HomeWindow.java
+++ src/window/HomeWindow.java
@@ -1,7 +1,12 @@
+package window;
+
+import game.Game;
+
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
