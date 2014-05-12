9d7f20519858c6fcc30e9ee938d31393a7d52a04
naraso <naraso@gmail.com>
2014-05-13 01:25:23 +1000

    add buttons for HomeWindow

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,19 +1,29 @@
-package window;
+import javax.swing.JFrame;
+
 
 /**
  * @author icedtrees
  *
  */
-public abstract class Window {
-    public Window(int width, int height) {
 
+public abstract class Window {
+	private JFrame window;
+	
+	
+    public Window(int width, int height) {
+        window = new JFrame();
+        window.setSize(width, height);
+    }
+    
+    public JFrame getWindow() {
+    	return window;
     }
     
     public void show() {
-    
+        window.setVisible(true);    
     }
     
     public void hide() {
-        
+        window.setVisible(false);        
     }
 }
