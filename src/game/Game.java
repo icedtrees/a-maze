33a4f94e348e191febd7f312d62830a7d16ead42
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2014-05-13 15:17:17 +1000

    Updated Game structure

--- src/game/Game.java
+++ src/game/Game.java
@@ -10,27 +10,42 @@ import window.MazeWindow;
 public class Game {
     public enum Command {
         PLAY_GAME,
+        INSTRUCTIONS,
         QUIT
     }
     
     private HomeWindow homeWindow;
     private MazeWindow mazeWindow;
-
-    public Game() {
-        homeWindow = new HomeWindow();
-        homeWindow.hide();
-    }
-    
-    public void run() {
-        homeWindow.show();
-        Command command = homeWindow.waitCommand();
-        // do something
-    }
     
     public static void main(String[] args) {
         Game game = new Game();
         game.run();
     }
     
+    public Game() {
+        homeWindow = new HomeWindow();
+        homeWindow.hide();
+    }
+    
+    public void run() {
+        currentWindow = homeWindow;
+        boolean running = true;
+        
+        while (running) {
+            homeWindow.show();
+            Command command = homeWindow.waitCommand();
+            if (Command == PLAY_GAME) {
+                mazeWindow.show();
+                homeWindow.hide();
+                currentWindow = mazeWindow;
+            } else if (Command == INSTRUCTIONS) {
+                currentWindow = instructionsWindow;
+                homeWindow.hide();
+                instructionsWindow.show()
+            } else if (Command == QUIT) {
+                running = false;
+            }
+        }
+    }
 
 }
