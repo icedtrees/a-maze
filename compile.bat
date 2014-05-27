4341a0d04c81f46e1c08992afbaf08c53a8fca59
icedtrees <e_equals_mass_speed_light_squared@hotmail.com>
2014-05-28 01:05:19 +1000

    Updated compile and run scripts, and added windows batch versions

---
+++ compile.bat
@@ -0,0 +1,22 @@
+rm -rf bin
+mkdir bin
+cd bin
+mkdir game
+mkdir pages
+mkdir maze
+
+cd maze
+mkdir autoplayer
+mkdir modification
+
+cd ../..
+
+javac -cp src -d "bin" src/game/*.java 
+javac -cp src -d "bin" src/pages/*.java
+javac -cp src -d "bin" src/maze/*.java   
+javac -cp src -d "bin/maze" src/maze/autoplayer/*.java
+javac -cp src -d "bin/maze" src/maze/modification/*.java
+
+cp img/* bin
+
+echo "done"
