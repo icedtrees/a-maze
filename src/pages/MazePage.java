5a7e97f04a5fad0c4f03bc0319ab20ab8da741ba
Leo Huang <crazysheep2302@gmail.com>
2014-05-29 02:12:39 +1000

    Change background color of maze and change title

--- src/pages/MazePage.java
+++ src/pages/MazePage.java
@@ -81,24 +81,24 @@ public class MazePage extends Page implements KeyListener{
 //    	int straightness = 900 - ((difficulty % 10) * 100);
 //    	int branching = 93 - ((difficulty % 10) * 10);
 		
-		int mazeHeight = 20;
+		int mazeHeight = 17;
 		int branching = 10;
 		int straightness = 0;
 		int startingTime = 60;
     	
     	java.util.List<Modification> mods = new java.util.ArrayList<Modification>();
-    	int torchFreq = 0;
     	int clockFreq = 2;
     	int bootsFreq = 2;
+    	int torchFreq = 3;
     	int scale = ((torchFreq + clockFreq + bootsFreq) / 100) + 1;
     	int numSpaces = (int) (mazeHeight * mazeHeight * Maze.DEFAULT_RATIO);
     	System.out.println(numSpaces);
     	System.out.println(bootsFreq * numSpaces / scale / 100);
     	
-//		mods.add(new FogMod(4, torchFreq * numSpaces / scale / 100));
+		mods.add(new FogMod(4, torchFreq * numSpaces / scale / 100));
 		mods.add(new ClockMod(clockFreq * numSpaces / scale / 100));
 		mods.add(new SpeedMod(bootsFreq * numSpaces / scale / 100));
- 		mods.add(new ShiftingWallsMod(10, 8));
+// 		mods.add(new ShiftingWallsMod(10, 8));
 		
 		mazeInfo = new MazeStats(1, startingTime);
 		
