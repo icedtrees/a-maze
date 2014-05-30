ca1652d2b09d858d81f10cc7ec19698659acb706
icedtrees <e_equals_mass_speed_light_squared@hotmail.com>
2014-05-30 20:06:32 +1000

    Added new level select page

---
+++ src/pages/LevelSelectPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+/**
+ * @author icedtrees
+ *
+ */
+public class LevelSelectPage extends Page {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     */
+    public LevelSelectPage() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see pages.Page#run()
+     */
+    @Override
+    public Result run() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
