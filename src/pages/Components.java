59c7cb48a7bce9ed3c55e9832341a7ffe8e7a78b
naraso <naraso@gmail.com>
2014-05-30 23:47:29 +1000

    Javadoc for Components

--- src/pages/Components.java
+++ src/pages/Components.java
@@ -5,7 +5,15 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+/**
+ * Components is a library of functions used in making the GUI. 
+ */
 public class Components {
+    /**
+     * Makes a translucent button with white text and a particular size.
+     * @param string to display on the button
+     * @return a button with the set properties
+     */
     public static JButton makeButton(String string) {
 		JButton button = new JButton(string) {
 			
@@ -44,11 +52,14 @@ public class Components {
     	button.setFont(new Font(font.getName(), Font.PLAIN, 20));
     	
     	button.setPreferredSize(new Dimension(200, 60));
-//    	button.setMinimumSize(new Dimension(200, 60));
-//    	button.setMaximumSize(new Dimension(200, 60));
         return button;
     }
     
+    /**
+     * Makes a label for titles - it has bigger font, which is white.
+     * @param string to display on the label.
+     * @return a label with the string shown in the centre
+     */
     public static JLabel makeTitle(String string) {
         JLabel titleLabel = new JLabel(string, JLabel.CENTER);
         Font font = titleLabel.getFont();
@@ -57,6 +68,11 @@ public class Components {
         return titleLabel;
     }
     
+    /**
+     * @param string The string displayed on the label
+     * @param fontSize The font size of the string
+     * @return a label with the specified font size. The text is in white.
+     */
     public static JLabel makeText(String string, int fontSize) {
         JLabel titleLabel = new JLabel(string, JLabel.CENTER);
         Font font = titleLabel.getFont();
@@ -65,32 +81,19 @@ public class Components {
         return titleLabel;
     }
     
+    /**
+     * @return A transparent JPanel
+     */
     public static JPanel makePanel() {
     	JPanel panel = new JPanel();
     	panel.setOpaque(false);
     	return panel;
     }
     
-    // currently not being used
-    public static JPanel makeTranslucentPanel() {
-    	JPanel panel = new JPanel() {
-			private static final long serialVersionUID = 1L;
-
-			// Overrides painting of background to make it translucent
-			@Override
-			protected void paintComponent(Graphics g) {
-				if (!isOpaque() && getBackground().getAlpha() < 255) {
-					g.setColor(getBackground());
-					g.fillRect(0, 0, getWidth(), getHeight());
-				}
-				super.paintComponent(g);
-			}
-    	};
-    	panel.setBackground(new Color(0, 0, 0, 50));
-    	panel.setOpaque(false);
-    	return panel;
-    }
-    
+    /**
+     * @param text to display for the checkBox
+     * @return a checkBox with white text and a transparent background
+     */
     public static JCheckBox makeCheckBox(String text) {
     	JCheckBox checkBox = new JCheckBox(text);
     	
@@ -98,13 +101,23 @@ public class Components {
     	Font font = checkBox.getFont();
     	checkBox.setForeground(Color.WHITE);
     	
-    	// gets rid of rectangle around text
+    	// gets rid of rectangle around text when it is clicked
     	checkBox.setFocusPainted(false);
     	checkBox.setFont(new Font(font.getName(), Font.PLAIN, 15));
     	
     	return checkBox;
     }
     
+    /**
+     * @param min Minimum value for slider
+     * @param max Maximum value for slider
+     * @param defaultValue default value to show on slider
+     * @param majTick Spacing of the major ticks
+     * @param minTick Spacing of the minor ticks
+     * @param length Length of the slider
+     * @return a horizontal slider with the given parameters, with the numbers
+     * in white font.
+     */
     public static JSlider makeJSlider(int min, int max, 
     	int defaultValue, int majTick, int minTick, int length) {
     	JSlider slider = new JSlider(JSlider.HORIZONTAL, min, max, defaultValue);
@@ -118,20 +131,4 @@ public class Components {
         return slider;
     }
     
-    //return button?? not used anywhere
-	public void addReturnButton(Page page) {
-        JPanel returnPanel = new JPanel();
-        returnPanel.setLayout(new FlowLayout());
-		
-        JButton returnBut = Components.makeButton("return");
-        returnBut.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	System.out.println("return to main menu");
-            	// page.setResult(Result.RETURN_HOME);
-            }
-        });
-		
-		returnPanel.add(returnBut);
-		page.add(returnPanel);
-	}
 }
